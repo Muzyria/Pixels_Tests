da59b7b2e7f2e21f9117721a5d59066e5cbb7626
FILA <fila090580@gmail.com>
2024-08-29 22:18:10 +0300

    added

--- tests/debug.py
+++ tests/debug.py
@@ -1,29 +1,28 @@
 import time
-
 from android_utils import *
 import subprocess
 
-# udid = 'dbe407da'
+
+def is_wifi_connected():
+    # Выполнение команды adb shell ip addr show wlan0
+    result = subprocess.run(["adb", "shell", "ip", "addr", "show", "wlan0"], capture_output=True, text=True)
+    # Проверка подключения Wi-Fi
+    if "inet " in result.stdout:
+        print("Устройство подключено к Wi-Fi")
+    else:
+        print("Устройство не подключено к Wi-Fi")
 
 
-def cart_of_hole_sleep_mode() -> None:
-    """Put device in off hole sleep"""
-    subprocess.run(
-        ['adb', '-s', udid, 'shell', 'am', 'broadcast', '-a', 'com.l1inc.yamatrack3d.action.powermanagement.not_on_hole_sleep'],
-        stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL
-    )
-    time.sleep(20)  # Период ожидания
-    print("Device in Off Hole Sleep Mode")
-
-
-
-def device_reboot() -> None:
-    """Reboot device"""
-    subprocess.run(['adb', '-s', udid, 'reboot'], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-
+def is_cellular_connected():
+    # Выполнение команды adb shell ip addr show rmnet_data0
+    result = subprocess.run(["adb", "shell", "ip", "addr", "show", "rmnet_data0"], capture_output=True, text=True)
+    if "inet " in result.stdout:
+        print("Устройство подключено к сотовому интернету")
+    else:
+        print("Устройство не подключено к сотовому интернету")
 
 
 if __name__ == '__main__':
-    get_udid()
-    cart_of_hole_sleep_mode()
-
+    # get_udid()
+    is_wifi_connected()
+    is_cellular_connected()
