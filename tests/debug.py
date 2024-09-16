95e4db45bc386720d86eb3e06f8c17f1017e8f7f
FILA <fila090580@gmail.com>
2024-09-16 22:26:44 +0300

    added logs test

--- tests/debug.py
+++ tests/debug.py
@@ -7,14 +7,20 @@ from android_utils import *
 import subprocess
 
 
-firmware_gps = dict(LC79D=('LC79DANR01A06S_BETA0322', 'LC79DANR01A07S'),
-                    LC79H=('LC79HALNR11A01S', 'LC79HALNR11A02S'),
-                    U_BLOX=('UDR1.00', 'UDR1.31'))
+def convert_time(time_str):
+    # Если длина строки 3, добавляем ведущий ноль
+    if len(time_str) == 3:
+        time_str = '0' + time_str
+
+    # Разбиваем строку на часы и минуты
+    hours = time_str[:-2]  # все символы кроме последних двух
+    minutes = time_str[-2:]  # последние два символа
+
+    # Возвращаем результат в формате HH:MM
+    return f"{hours}:{minutes}"
 
 
-def get_list_gps_version_ota(device_gps_version: str):
-    for k, v in firmware_gps.items():
-        return k if device_gps_version in v else None
+
 
 
 
@@ -22,5 +28,7 @@ if __name__ == '__main__':
     # get_udid()
     # android_utils.get_wakefulness_status()
     # android_utils.wake_up_device()
+    # Пример использования
+    time_str = "103"
+    print(convert_time(time_str))  # Вывод: 01:23
 
-    print(get_list_gps_version_ota('LC79DANR01A06S_BETA0322'))
