30529f57de7e132dc1b4f4e0a7e298e8b1aed48d
FILA <fila090580@gmail.com>
2024-10-24 12:37:11 +0300

    added matrix test

--- pages_chrome/asset_tracker_page_360.py
+++ pages_chrome/asset_tracker_page_360.py
@@ -1,5 +1,5 @@
 from pages_chrome import PageChrome
-
+from selenium.webdriver.support.select import Select
 
 class AssetTrackerSyncWise360(PageChrome):
 
@@ -8,6 +8,9 @@ class AssetTrackerSyncWise360(PageChrome):
     SPEED_AND_BRAKING_MODE_BUTTON = ("xpath", "//p[text()='Speed And Braking Mode']")
     CLOSE_MODAL_BUTTON = ("xpath", '//button[@class="closeModal"]')
     SAVE_MODAL_BUTTON = ("xpath", '//button[@class="btn"]')
+
+    SELECT_SPEED = ("xpath", '//div[@class="form-group"][1]/mat-select')
+    SELECT_BRAKE = ("xpath", '//div[@class="form-group"][2]/mat-select')
 
 
     def __init__(self) -> None:
@@ -21,9 +24,19 @@ class AssetTrackerSyncWise360(PageChrome):
 
     def click_save_modal_button(self):
         self.visibility_of_element_located(self.SAVE_MODAL_BUTTON).click()
+        self.check_spinner_is_invisible()
+
+    def select_dropdown_speed(self, value: str):
+        self.visibility_of_element_located(self.SELECT_SPEED).click()
+        self.visibility_of_element_located(("xpath", f'//span[text()=" {value}mph "]')).click()
+
+    def select_dropdown_brake(self, value: str):
+        self.visibility_of_element_located(self.SELECT_BRAKE).click()
+        self.visibility_of_element_located(("xpath", f'//span[text()=" {value} "]')).click()
 
 
 
     def check_spinner_is_invisible(self):
+        self.visibility_of_element_located(self.SPINNER)
         self.invisibility_of_element_located(self.SPINNER)
-        print("spinner is invisible on general setting page")
+        print("spinner is invisible on asset tracker page")
--- tests/matrix_mode_values_testing/test_matrix_mode_values.py
+++ tests/matrix_mode_values_testing/test_matrix_mode_values.py
@@ -2,7 +2,7 @@ import pytest
 from pages_chrome import PageChrome
 from pages_android import Page
 import time
-
+import subprocess
 from framework_appium.driver_appium import DriverAppium
 
 from framework_chrome.driver_chrome import DriverChrome
@@ -18,26 +18,112 @@ import android_utils
 
 
 class TestMatrixMode:
+    @staticmethod
+    def run_adb_logcat():
+        # Запуск команды adb logcat
+        process = subprocess.Popen('adb logcat -d', stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+        output, _ = process.communicate()
+        return output.decode('utf-8', errors='ignore')
+
+    @staticmethod
+    def check_for_message(message_to_find, interval=30, timeout=600):
+        """
+        Проверка adb логов на наличие определённого сообщения.
+
+        :param message_to_find: Сообщение, которое необходимо найти.
+        :param interval: Интервал в секундах между выполнением команды adb logcat.
+        :param timeout: Время ожидания в секундах до прекращения поиска.
+        :return: True, если сообщение найдено, False - если тайм-аут истек.
+        """
+
+        start_time = time.time()
+        start_time_readable = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(start_time))
+        print(f"Начало ожидания сообщения в {start_time_readable}, тайм-аут: {timeout} секунд")
+
+        last_adb_execution_time = 0  # Время последнего выполнения команды
+
+        while time.time() - start_time < timeout:
+            current_time = time.time()
+
+            # Выполняем adb logcat с интервалом
+            if current_time - last_adb_execution_time >= interval:
+                # self.adb_command.swipe_screen(100, 500, 200, 500, 250)
+                # self.adb_command.swipe_screen(200, 500, 100, 500, 250)
+                android_utils.swipe_screen_up_to_down()
+                android_utils.swipe_screen_down_to_up()
+                # android_utils.touch_screen_by_coordinate()
+                # print("-----ADB COMMAND----------------------------------------------")
+                last_adb_execution_time = current_time
+                try:
+                    output = TestMatrixMode.run_adb_logcat()
+                    if message_to_find in output:
+                        end_time = time.time()
+                        end_time_readable = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(end_time))
+                        duration = int(end_time - start_time)
+                        print(
+                            f"Сообщение '{message_to_find}' найдено в {end_time_readable}, время ожидания: {duration} секунд")
+                        return True
+                except Exception as e:
+                    print(f"Ошибка при поиске сообщения в логе: {e}")
+
+            # Ждем 1 секунду перед повторной проверкой
+            time.sleep(1)
+            android_utils.device_send_coordinate("50.08066353581793, 36.23103477093396")
+
+        print(f"Сообщение '{message_to_find}' не найдено за {timeout} секунд.")
+        return False
 
     # 360 ----------------------------------------------------------------------------------------------------
     @staticmethod
-    def set_matrix_mode_360():
+    def set_matrix_mode_360(speed_value: str, brake_value: str):
         LoginPageSyncWise360.open(LoginPageSyncWise360.PAGE_URL)
         LoginPageSyncWise360().enter_login().enter_password().click_login_button()
         CourseMapSyncWise360().click_settings_icon()
         GeneralSettingSyncWise360().click_asset_tracker_button()
         AssetTrackerSyncWise360().click_speed_and_braking_mode_button()
-        AssetTrackerSyncWise360().click_close_modal_button()
-        time.sleep(3)
+
+        AssetTrackerSyncWise360().select_dropdown_speed(speed_value)
+        AssetTrackerSyncWise360().select_dropdown_brake(brake_value)
+
+        AssetTrackerSyncWise360().click_save_modal_button()
+        time.sleep(10)
+
         LoginPageSyncWise360().click_logout_button()
         time.sleep(3)
 
-
-
-    def test_matrix_mode_check_values(self, request) -> None:
+    @pytest.mark.parametrize("speed, brake, matrix_mode", [
+                    ("10", "Max", "1"),
+                    ("11", "Max", "2"),
+                    ("12", "Max", "3"),
+                    ("13", "Max", "4"),
+                    ("14", "Max", "5"),
+                    ("10", "High", "6"),
+                    ("11", "High", "7"),
+                    ("12", "High", "8"),
+                    ("13", "High", "9"),
+                    ("14", "High", "10"),
+                    ("10", "Medium", "11"),
+                    ("11", "Medium", "12"),
+                    ("12", "Medium", "13"),
+                    ("13", "Medium", "14"),
+                    ("14", "Medium", "15"),
+                    ("10", "Low", "16"),
+                    ("11", "Low", "17"),
+                    ("12", "Low", "18"),
+                    ("13", "Low", "19"),
+                    ("14", "Low", "20"),
+                    ("10", "Lowest", "21"),
+                    ("11", "Lowest", "22"),
+                    ("12", "Lowest", "23"),
+                    ("13", "Lowest", "24"),
+                    ("14", "Lowest", "25"),
+    ])
+    def test_matrix_mode_check_values(self, request, speed, brake, matrix_mode) -> None:
         print()
         print(f"START {request.node.name}")
 
-        TestMatrixMode.set_matrix_mode_360()
+        TestMatrixMode.set_matrix_mode_360(speed, brake)
+        TestMatrixMode.check_for_message(f"CartCommand MATRIX_MODE = {matrix_mode}")
+        time.sleep(10)
 
         print(f"FINISH {request.node.name}")
