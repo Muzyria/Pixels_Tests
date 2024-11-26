88e05e3ac5cc5df484cf3aff58f1643f4ddf01d3
FILA <fila090580@gmail.com>
2024-11-26 18:34:31 +0200

    added

--- tests/config_data.py
+++ tests/config_data.py
@@ -1,13 +1,13 @@
-firmware = {"device_id": "S10150000211018049",
-            "device_name": "49",
-            "device_os": None,
-            "device_apk": None,
-            "os_current": "2.20.0",
-            "os_to_update": "2.15.8_E",
-            "apk_current": "2.2.14",
-            "apk_to_update": "2.2.12",
-            "device_gps": None
-            }
+# firmware = {"device_id": "S10150000211018049",
+#             "device_name": "49",
+#             "device_os": None,
+#             "device_apk": None,
+#             "os_current": "2.20.0",
+#             "os_to_update": "2.15.8_E",
+#             "apk_current": "2.2.14",
+#             "apk_to_update": "2.2.12",
+#             "device_gps": None
+#             }
 
 firmware_gps = dict(LC79D=('LC79DANR01A06S_BETA0322', 'LC79DANR01A07S'),
                     LC79H=('LC79HALNR11A01S', 'LC79HALNR11A02S'),
@@ -15,12 +15,12 @@ firmware_gps = dict(LC79D=('LC79DANR01A06S_BETA0322', 'LC79DANR01A07S'),
 
 # ---------------------------------------------
 
-# firmware = {"device_id": "L10115001805220010",
-#             "device_name": "10",
-#             "device_os": None,
-#             "device_apk": None,
-#             "os_current": "2.20.0",
-#             "os_to_update": "2.15.8_E",
-#             "apk_current": "2.2.12",
-#             "apk_to_update": "2.2.11"
-#             }
+firmware = {"device_id": "L10116001811250A52",
+            "device_name": "52",
+            "device_os": None,
+            "device_apk": None,
+            "os_current": "1.55.2",
+            "os_to_update": "1.54.0",
+            "apk_current": "2.2.14",
+            "apk_to_update": "2.2.12"
+            }
